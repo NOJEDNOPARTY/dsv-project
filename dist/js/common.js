19c4ea4a04d6b7e0339ca3d5bbe613353fcabdb3
NOJEDNOPARTY <bordyuh@gmail.com>
2020-02-27 15:43:11 +0200

    add 3 blocks of main page

---
+++ dist/js/common.js
@@ -0,0 +1,85 @@
+var common = {
+	init: function() {
+		common.main();
+		common.carousel();
+	},
+	main: function(){
+
+		var bLazy = new Blazy({});
+
+	},
+	carousel: function(){
+
+		$('.banner').owlCarousel({
+			loop:true,
+			margin: 0,
+			nav: false,
+			dots: true,
+			lazyLoad: true,
+			// autoplay:true,
+    		autoplayTimeout: 5000,
+			autoplayHoverPause:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1000:{
+					items:1
+				}
+			}
+		});
+
+		$('.products-slider').owlCarousel({
+			loop:true,
+			margin: 30,
+			nav: true,
+			dots: true,
+			lazyLoad: true,
+			// autoplay:true,
+    		autoplayTimeout: 5000,
+			autoplayHoverPause:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:2
+				},
+				1000:{
+					items:4
+				}
+			}
+		})
+		$('.videos-slider').owlCarousel({
+			loop:true,
+			margin: 30,
+			nav: true,
+			dots: false,
+			lazyLoad: true,
+			// autoplay:true,
+    		autoplayTimeout: 5000,
+			autoplayHoverPause:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:2
+				},
+				1000:{
+					items:3
+				}
+			}
+		})
+			
+	}
+};
+
+(function() {
+	common.init();
+}());
+
+
